82727dd1ff4eff210fe147976a248c54968109a9
Ethan Bundy <gunderodd@hotmail.com>
2019-06-02 05:46:59 -0700

    all basics from MT, starting to add project1 specs

--- my_app/models.py
+++ my_app/models.py
@@ -1,7 +1,9 @@
-from my_app import db
+from my_app import db, login
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -11,6 +13,13 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+# update the posts to be reviews that show on the book's page
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -19,3 +28,15 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+# ...and add another table for the books themselves
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ISBN = db.Column(db.Integer)
+    title = db.Column(db.String)
+    author = db.Column(db.String)
+    year = db.Column(db.Integer)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
