46f47f7b2db75e35832fe381de9fdc170c6373f7
Ethan Bundy <gunderodd@hotmail.com>
2019-08-12 02:22:29 -0700

    heroku deplyment changes

--- my_microblog.py
+++ my_microblog.py
@@ -1,5 +1,9 @@
-from my_app import app, db, cli
+from my_app import create_app, db, cli
 from my_app.models import User, Post
+
+app = create_app()
+cli.register(app)
+
 
 @app.shell_context_processor
 def make_shell_context():
